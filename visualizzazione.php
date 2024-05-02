94265cadf532efa6c93a5278e1950149487b41b5
Righi <its.righi@righilab.local>
2024-05-02 11:44:42 +0200

    V2 prova

--- visualizzazione.php
+++ visualizzazione.php
@@ -1,9 +1,14 @@
 <?php
-$folder = "images/";
+$folder = ".\images\\";
 
 $file = glob($folder . "*.{jpg,jpeg,png,gif}",GLOB_BRACE);
 
+
 foreach($file as $meme){
-    echo '<img src="' . $image . '" alt="meme" />';
+    
+    echo '<div class="post">
+            <img src="' . $meme . '" alt="meme" />
+          </ div>  
+            ';
 }
 
