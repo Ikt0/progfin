682d1a4caa648c889e2fc5a1147f128a59ed6185
LucaDS7 <its.righi@righilab.local>
2024-05-09 11:09:07 +0200

    Fix bugs

--- registration.php
+++ registration.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $stmt = $pdo->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $stmt = $pdo->prepare("INSERT INTO utente (username, password) VALUES (?, ?)");
     $stmt->execute([$username, $password]);
     echo "Registration successful!";
 
